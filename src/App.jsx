f094c9c3a00f62b609a7bd05d205f8253e07ad92
River Wang <urfdvw@gmail.com>
2023-08-20 18:46:50 -0400

    button text and change formData on the go

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,33 @@ import schema from "./schema.json";
 import uiSchema from "./uiSchema.json";
 
 function App() {
+    const [formData, setFormData] = useState({});
+
     const handleSubmit = (e) => {
         console.log(e.formData);
     };
     return (
         <>
+            <button
+                onClick={() => {
+                    setFormData({
+                        accounts: [
+                            {
+                                website: "hi",
+                            },
+                        ],
+                    });
+                }}
+            >
+                change data
+            </button>
             <Form
+                formData={formData}
                 schema={schema}
                 uiSchema={uiSchema}
                 validator={validator}
                 onSubmit={handleSubmit}
+                omitExtraData={true}
             />
         </>
     );
