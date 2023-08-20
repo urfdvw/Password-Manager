ed0007183bcb0690301305b485b9d1b03239fdcf
River Wang <urfdvw@gmail.com>
2023-08-20 17:28:38 -0400

    json schema form

--- log/readme.md
+++ log/readme.md
@@ -3,3 +3,7 @@
 - `npm install vite-plugin-singlefile --save-dev`
     - `npm run build` to publish to single page
     - ref: https://github.com/richardtallent/vite-plugin-singlefile
+- `npm install @rjsf/core @rjsf/utils @rjsf/validator-ajv8 --save`
+    - ref: https://rjsf-team.github.io/react-jsonschema-form/docs/
+    - dependency: `npm install tslib`
+    
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
+import Form from "@rjsf/core";
+import validator from "@rjsf/validator-ajv8";
+
+const schema = {
+    title: "Todo",
+    type: "object",
+    required: ["title"],
+    properties: {
+        title: { type: "string", title: "Title", default: "A new task" },
+        done: { type: "boolean", title: "Done?", default: false },
+    },
+};
 
 function App() {
-    const [count, setCount] = useState(0);
-
+    const handleSubmit = (e) => {
+        console.log(e.formData);
+    };
     return (
         <>
-            <button onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-            </button>
+            <Form
+                schema={schema}
+                validator={validator}
+                onSubmit={handleSubmit}
+            />
         </>
     );
 }
